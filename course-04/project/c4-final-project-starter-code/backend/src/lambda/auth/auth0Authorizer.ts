7f23e0dc061553b278a51d1e72b64e700f647950
Enock Tito <enocktito@MacBook-Pro-de-Enock.local>
2022-10-01 17:30:13 +0200

    keep

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,16 +3,17 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
+// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth');
+// import JwksClient from 'jwks-client'
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-nh-hm0e8.us.auth0.com/.well-known/jwks.json'
+// const jwksUrl = 'https://dev-nh-hm0e8.us.auth0.com/.well-known/jwks.json'
 const cert = `-----BEGIN CERTIFICATE-----
 MIIDDTCCAfWgAwIBAgIJIfl+PKO7xXsSMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
 BAMTGWRldi1uaC1obTBlOC51cy5hdXRoMC5jb20wHhcNMjIwOTI4MjAzNjAxWhcN
@@ -76,10 +77,32 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  console.log(jwt);
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+  // const client = jwksClient({
+  //   strictSsl: true, // Default value
+  //   jwksUri: jwksUrl
+  // });
+  // const options = {
+  //     strictSsl: true, // Default value
+  //     jwksUri: jwksUrl
+  //   }
+  // const client = new JwksClient(options);
+  // client.getJwks((err, res) =>{
+  //   if (err) {
+  //     console.log(err);
+  //     return null;
+  //   }
+  //   console.log(res);
+  // })
+  
+  // const kid = 'RkI5MjI5OUY5ODc1N0Q4QzM0OUYzNkVGMTJDOUEzQkFCOTU3NjE2Rg';
+  // client.getSigningKey(kid, (err, key) => {
+  //   const signingKey = key.publicKey;
+  // });
   
   return verify( token,cert, {algorithms: ['RS256']} ) as JwtPayload
 }
